e0c992962c3f916ac573e0ca90257da076aaa802
Charlie Hebert <charlie.hebert92@gmail.com>
2014-12-11 13:15:54 +0000

    Update home page again.

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -10,12 +10,13 @@
     <section>
         <div class="container">
             <div class="row">
-                <div class="col-lg-12">
+                <div class="col-lg-8">
                     <h1 class="section-heading">Hi, I'm Charlie.</h1>
                     <p class="lead section-lead">I'm a 3rd year Computer Science and Business student
                     at Trinity College Dublin. I am particularly interested in <span class="text-info">
-                    web development</span> and <span class="text-info">entrepreneurship</span> and
-                    currently I'm working on improving my skills in these areas as much as possible.</p>
+                    web development</span> and <span class="text-info">entrepreneurship</span>.
+                    I'm currently working on improving my skills in these areas as much as possible,
+                    in addition to my studies.</p>
                     <p class="lead section-lead">The purpose of this site is to keep a record of what I've
                     been up to and what I'm currently working on.</p>
                 </div>
@@ -27,7 +28,7 @@
     <section>
         <div class="container">
             <div class="row">
-                <div class="col-lg-12">
+                <div class="col-lg-8">
                     <h1 class="section-heading">What can I do?</h1>
                     <h3>Strong</h3>
                     <ul>
@@ -87,17 +88,15 @@
     <section>
         <div class="container">
             <div class="row">
-                <div class="col-lg-12">
+                <div class="col-lg-8">
                     <h1 class="section-heading">What am I working on?</h1>
                     <ul>
                         <li>Javascript</li>
                         <ul>
                             <p>Currently working through a <a href="http://eloquentjavascript.net/">
                             great book </a>that covers the fundamentals of the Javascript language
-                            and its current ecosystem.<p>
-
-                            <p>Once complete, I hope to get stuck into Node and one of the popular
-                            frontend frameworks like Ember or Backbone.</p>
+                            and its current ecosystem. Once complete, I hope to get stuck into Node
+                            and one of the popular frontend frameworks like Ember or Backbone.</p>
                         </ul>
 
                         <li>Back-End Development</li>
