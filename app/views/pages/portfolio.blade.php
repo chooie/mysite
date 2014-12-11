0d9f86f5e6b7bfe57cad599d8289d58fc29148b2
Charlie Hebert <charlie.hebert92@gmail.com>
2014-12-11 14:10:25 +0000

    Update portfolio page and home page. Disable debigging for production.

--- app/views/pages/portfolio.blade.php
+++ app/views/pages/portfolio.blade.php
@@ -5,5 +5,19 @@
 @stop
 
 @section('content')
-More to come.
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8">
+                <h1 class="section-heading">E-Commerce Site</h1>
+                <p class="lead section-lead"> A group university assignment. I was responsible for
+                all of the backend and was also a key member and mentor for the frontend. The application
+                is obviously not functional as a fully-fledged e-commerce site but it was a great learning
+                experience.</p>
+                <p class="lead section-lead">
+                    You can find it <a href="http://shop.charliehebert.com">here</a>.
+                </p>
+            </div>
+        </div>
+    </div>
 @stop
