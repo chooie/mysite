4589b8440b16abf717f4e20879f45810723bffc0
Charlie Hebert <charlie.hebert92@gmail.com>
2015-04-20 20:21:39 +0100

    Remove use of database

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -19,8 +19,7 @@ return array(
 	*/
 
 	'connections' => array(
-
-		'mysql' => array(
+		/*'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
 			'database'  => 'homestead',
@@ -29,19 +28,6 @@ return array(
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
-		),
-
-		'pgsql' => array(
-			'driver'   => 'pgsql',
-			'host'     => 'localhost',
-			'database' => 'homestead',
-			'username' => 'homestead',
-			'password' => 'secret',
-			'charset'  => 'utf8',
-			'prefix'   => '',
-			'schema'   => 'public',
-		),
-
+		)*/
 	),
-
 );
